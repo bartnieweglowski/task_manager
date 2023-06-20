08af33a3edd2d2a034a1403019b63ac0564ff5a6
bartnieweglowski <bartnieweglowski@gmail.com>
2023-06-20 17:04:13 +0200

    Routing and navigation update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Board from './Board/Board';
+import Communication from './Communication/Communication';
+import CreateProject from './Create_project/Create_project';
+import ManageTasks from './Manage_tasks/Manage_tasks';
+import ProgressMonitoring from './Progress_monitoring/Progress_monitoring';
+import Notifications from './Notifications/Notifications';
+import TimeTracking from './TimeTracking/TimeTracking';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className="title">
+          <h1>Project Management Platform</h1>
+        </div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/board">Board</Link>
+              </li>
+              <li>
+                <Link to="/create-project">Create Project</Link>
+              </li>
+              <li>
+                <Link to="/manage-tasks">Manage Tasks</Link>
+              </li>
+              <li>
+                <Link to="/progress-monitoring">Progress Monitoring</Link>
+              </li>
+              <li>
+                <Link to="/communication">Communication</Link>
+              </li>
+              <li>
+                <Link to="/notifications">Notifications</Link>
+              </li>
+              <li>
+                <Link to="/time-tracking">Time Tracking</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/board" element={<Board />} />
+          <Route path="/create-project" element={<CreateProject />} />
+          <Route path="/manage-tasks" element={<ManageTasks />} />
+          <Route path="/progress-monitoring" element={<ProgressMonitoring />} />
+          <Route path="/communication" element={<Communication />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/time-tracking" element={<TimeTracking />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
