678291f9c99666e89d6a75ffdab87fa28718c7a0
Bartłomiej Niewęglowski <bartnieweglowski@gmail.com>
2023-11-15 19:34:02 +0100

    add theme color switcher

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,70 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Board from './Board/Board';
 import Communication from './Communication/Communication';
 import CreateProject from './Create_project/Create_project';
 import ManageTasks from './Manage_tasks/Manage_tasks';
 import ProgressMonitoring from './Progress_monitoring/Progress_monitoring';
 import Notifications from './Notifications/Notifications';
-import TimeTracking from './TimeTracking/TimeTracking';
+import TimeTracking from './TimeTracking/TimeTracking'; 
+import './ColorSwitcher/switcher.scss'
+
 
 const App = () => {
+
+  const [colorTheme, setColorTheme] = useState('theme-white');
+  
+  useEffect(()=> {
+      const currentThemeColor = localStorage.getItem('theme-color');
+  
+      if (currentThemeColor) {
+          setColorTheme(currentThemeColor);
+      }
+  }, []);
+  
+  const handleClick = (theme) => {
+  setColorTheme(theme);
+  localStorage.setItem('theme-color', theme)
+  }
+  
   return (
     <Router>
-      <div>
+      <div className={`App ${colorTheme}`}>
+        <div className='title-container'>
         <div className="title">
           <h1>Project Management Platform</h1>
+          </div>
+          <div className='theme-options-container'>
+          <div className="theme-options">
+            <div id='theme-white'
+            onClick={() => handleClick('theme-white')}
+            className={`${colorTheme === 'theme-white' ? 'active' : ''}`}
+            />
+            <div id="theme-blue"
+            onClick={() => handleClick('theme-blue')}
+            className={`${colorTheme === 'theme-blue' ? 'active' : ''}`}
+            />
+            <div id="theme-orange"
+            onClick={() => handleClick('theme-orange')}
+            className={`${colorTheme === 'theme-orange' ? 'active' : ''}`}
+            />
+            <div id="theme-purple"
+            onClick={() => handleClick('theme-purple')}
+            className={`${colorTheme === 'theme-purple' ? 'active' : ''}`}
+            />
+            <div id="theme-green"
+            onClick={() => handleClick('theme-green')}
+            className={`${colorTheme === 'theme-green' ? 'active' : ''}`}
+            />
+            <div id="theme-black"
+            onClick={() => handleClick('theme-black')}
+            className={`${colorTheme === 'theme-black' ? 'active' : ''}`}
+            />
+          </div>
         </div>
-        <header>
+      </div>
+        <header>  
           <nav>
             <ul>
               <li>
